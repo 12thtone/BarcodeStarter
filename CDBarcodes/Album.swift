ea67f23bcaefed9d018cd68dd885113eea5de716
Matt Maher <maher@12thtone.com>
2016-01-29 12:05:34 -0500

    Initial commit

---
+++ CDBarcodes/Album.swift
@@ -0,0 +1,8 @@
+//
+//  Album.swift
+//  CDBarcodes
+//
+//  Created by Matthew Maher on 1/29/16.
+//  Copyright © 2016 Matt Maher. All rights reserved.
+//
+
---
+++ CDBarcodes/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  CDBarcodes
+//
+//  Created by Matthew Maher on 1/29/16.
+//  Copyright © 2016 Matt Maher. All rights reserved.
+//
+
+import Foundation
