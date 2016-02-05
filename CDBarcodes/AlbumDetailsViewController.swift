9868618fdab4cb7f0dd7ebf798eb6c7827cd8c5e
Matt Maher <maher@12thtone.com>
2016-02-05 13:07:47 -0500

    Fixes

--- CDBarcodes/AlbumDetailsViewController.swift
+++ CDBarcodes/AlbumDetailsViewController.swift
@@ -15,7 +15,6 @@ class AlbumDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
 
--- CDBarcodes/BarcodeReaderViewController.swift
+++ CDBarcodes/BarcodeReaderViewController.swift
@@ -13,15 +13,10 @@ class BarcodeReaderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
-    }
-
-    @IBAction func cancelTapped(sender: AnyObject) {
-        
     }
 }
